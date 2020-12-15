c3c9d033d2882b3c73e503d47cd1785cf8a4e57a
butnaruvalentin98@gmail.com <butnaruvalentin98@gmail.com>
2020-12-15 03:04:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Agent_Mesagerie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
